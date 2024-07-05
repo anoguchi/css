7caf4e9b6dd51554cf189f51cf0437961a4d1c58
Alberto Noguchi <alberto2611@gmail.com>
2024-07-04 23:52:02 -0300

    add: responsible workshop solution

---
+++ responsive/src/components/MobileMenu/MobileMenu.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
+
+import { WEIGHTS, QUERIES } from "../../constants";
+
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+
+const MobileMenu = ({ isOpen, onDismiss }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Content>
+        <Filler />
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close" />
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+        </CloseButton>
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+        <Footer>
+          <a href="/terms">Terms and Conditions</a>
+          <a href="/privacy">Privacy Policy</a>
+          <a href="/contact">Contact Us</a>
+        </Footer>
+      </Content>
+    </Overlay>
+  );
+};
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  inset: 0;
+  background: var(--color-backdrop);
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Content = styled(DialogContent)`
+  background: white;
+  height: 100%;
+  width: 300px;
+  display: flex;
+  padding: 32px;
+  flex-direction: column;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+  position: absolute;
+  top: 10px;
+  right: 0;
+  padding: 16px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const NavLink = styled.a`
+  color: var(--color-gray-900);
+  font-weight: ${WEIGHTS.medium};
+  text-decoration: none;
+  font-size: 1.125rem;
+  text-transform: uppercase;
+
+  &:first-of-type {
+    color: var(--color-secondary);
+  }
+`;
+const Footer = styled.footer`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+const Filler = styled.div`
+  flex: 1;
+`;
+export default MobileMenu;
---
+++ responsive/src/components/ShoeGrid/ShoeGrid.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+import { QUERIES } from '../../constants';
+
+import SHOES from '../../data';
+import ShoeCard from '../ShoeCard';
+
+const ShoeGrid = () => {
+  return (
+    <Wrapper>
+      {SHOES.map((shoe) => (
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
+  /*
+    Alternatively, if we can't use 'gap', we could set:
+
+    margin: -16px;
+
+    We'd also need to set this on the ShoeWrapper:
+
+    margin: 16px;
+  */
+`;
+
+const ShoeWrapper = styled.div`
+  min-width: 25%;
+  flex: 1;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    min-width: 45%;
+  }
+`;
+
+export default ShoeGrid;
